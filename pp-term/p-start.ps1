d418ff9cfa02273db4cd36b9510475dd238f5a2a
Peharge <Julian.kropff@gmx.de>
2025-05-23 19:50:28 +0200

    update pp-term -> p-start.ps1

--- pp-term/p-start.ps1
+++ pp-term/p-start.ps1
@@ -93,7 +93,7 @@ if (!(Test-Path $logDir)) {
 
 try {
     # Start-Log
-    Write-Log INFO "Run PP-Term script with maximum CPU affinity..."
+    Write-Log INFO "Run PP-Term 5 script with maximum CPU affinity..."
 
     # Existenz prüfen
     if (!(Test-Path $PythonExe)) {
