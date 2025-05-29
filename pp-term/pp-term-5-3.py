63007dc0e1912f2442cfc3bba7aa30cd8265ecf2
Peharge <Julian.kropff@gmx.de>
2025-05-29 13:34:23 +0200

    update pp-term -> pp-term-5-3.py

--- pp-term/pp-term-5-3.py
+++ pp-term/pp-term-5-3.py
@@ -4706,6 +4706,24 @@ def handle_special_commands(user_input):
             print(f"[{timestamp()}] [ERROR] executing pcf command: {e}")
         return True
 
+    if user_input.startswith("pcfo "):
+        user_input = user_input[4:].strip()
+        current_dir = Path.cwd().resolve()
+
+        command = f"mkdir {user_input}"
+
+        process = subprocess.Popen(command, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, shell=True,
+                                   text=True)
+
+        try:
+            print(f"[{timestamp()}] [INFO] Folder created: {current_dir}\\{user_input}")
+            process.wait()
+        except KeyboardInterrupt:
+            print(f"[{timestamp()}] [INFO] Cancellation by user.")
+        except subprocess.CalledProcessError as e:
+            print(f"[{timestamp()}] [ERROR] executing pcf command: {e}")
+        return True
+
     if user_input.startswith("pff "):
         user_input = user_input[4:].strip()
         current_dir = Path.cwd().resolve()
