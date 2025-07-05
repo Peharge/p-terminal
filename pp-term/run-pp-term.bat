0392044dd3057c5f86b417b1423f7035ff5de29a
Peharge <Julian.kropff@gmx.de>
2025-07-05 11:02:25 +0200

    update pp-term -> run-pp-term.bat

--- pp-term/run-pp-term.bat
+++ pp-term/run-pp-term.bat
@@ -113,7 +113,7 @@ wmic logicaldisk get name, description, filesystem, size, freespace
 echo Logged in Users:
 query user
 echo.
-echo Initializing PP-Terminal 5
+echo Initializing PP-Terminal 6
 echo Gooo...
 echo.
 
