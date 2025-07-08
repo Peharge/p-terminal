6c2585d44dac2663c0d7dbd24dee8a55779f4ecc
Peharge <Julian.kropff@gmx.de>
2025-07-08 12:29:35 +0200

    update pp-term -> ...

--- pp-term/pp-commands/update-pp-term-venv.py
+++ pp-term/pp-commands/update-pp-term-venv.py
@@ -163,7 +163,7 @@ bold = "\033[1m"
 def confirm_action(message: str) -> bool:
     """Fordert den Benutzer zur Bestätigung auf."""
     while True:
-        response = input(f"{message} [y/n]:").strip().lower()
+        response = input(f"{message} [y/n]: ").strip().lower()
         if response in ["y", "yes"]:
             return True
         elif response in ["n", "no"]:
