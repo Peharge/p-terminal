8db9b3d4e820cc29d218c08517ce9041b9a31f34
Peharge <Julian.kropff@gmx.de>
2025-07-10 17:09:43 +0200

    update pp-term -> pp-term-6-4.py

--- pp-term/pp-term-6-4.py
+++ pp-term/pp-term-6-4.py
@@ -28522,7 +28522,7 @@ def main():
 
             elif user_input.startswith("pov "):
                 user_input = user_input[4:]
-                print(f"[{timestamp()}] [INFO] Please note that the pov command supports only CMD commands, not the PP terminal. It was specifically developed for managing virtual environments, so not all commands may be compatible.")
+                print(f"[{timestamp()}] [INFO] Please note that the pov command supports only CMD commands, no PP terminal and the command was specifically developed for managing virtual environments, so not all CMD commands may be compatible.")
                 run_command_ov(user_input, shell=True)
 
             elif user_input.startswith("pp "):
